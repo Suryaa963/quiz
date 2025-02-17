497a48f13b8b8fbe29a723945370482b28648474
suryaprakash <Suryaprakash.G@blackstraw.ai>
2025-02-17 15:25:27 +0530

    commentedout leaderboard api

--- api/leaderboard.py
+++ api/leaderboard.py
@@ -11,23 +11,21 @@ bp_leaderboard = Blueprint('leaderboard', __name__)
 @jwt_required()
 
 def fetchAllLeaders():
-    print('hiii')
-    quizz_titles =  Quiz.query.with_entities(Quiz.id, Quiz.title).all()
-    print('quizzes',quizz_titles)
-    return jsonify({
-        "result": [{"id": quiz.id,"title": quiz.title} for quiz in quizz_titles]
-    }),200
+    # todo
+
+    # print('hiii')
+    # quizz_titles =  Quiz.query.with_entities(Quiz.id, Quiz.title).all()
+    # print('quizzes',quizz_titles)
+    # return jsonify({
+    #     "result": [{"id": quiz.id,"title": quiz.title} for quiz in quizz_titles]
+    # }),200
 
 # fetching complete answer and options for a particular quiz id
 
-@bp_leaderboard.route('/<int:quizid>',methods = ['GET'])
-@jwt_required()
-def fetchQuizQuestions(quizid):
-    print('hello',quizid )
-    quizz =  Quiz.query.get(quizid)
-    quiz_questions = quizz.questions
-    print('quizzes',quizz)
-    return jsonify({
-        "result": [question.toDict() for question in quiz_questions]
-    }),200
+
+ @bp_leaderboard.route('/<int:quizid>',methods = ['GET'])
+ @jwt_required()
+ def fetchUserDetail():
+  pass
+ #todo
  
